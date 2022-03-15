c0cbfec45a1535c5f438f0f5bfbfa92e800c7a4b
azharul77 <azharulamin.cse@gmail.com>
2022-03-16 01:29:46 +0600

    test case result added in log

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,7 +1,9 @@
 from odoo.tests import TransactionCase
 from odoo.tests import common
 import unittest
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class TestModule(common.TransactionCase):
     def setUp(self):
@@ -13,8 +15,8 @@ class TestModule(common.TransactionCase):
             'age': 25,
             'mobile': '01761970239',
             'email': 'azharulamin.cse@gmail.com'
-
         })
+
         test_ctc = self.env['rest.staff'].create({
             'hand_salary': 15000,
             'epf_esi': 5000,
@@ -22,9 +24,16 @@ class TestModule(common.TransactionCase):
         print(test_staff, test_ctc+"Staff created")
 
     def test_limitation(self):
-        hand_salary_status = self.env['rest.staff'].search([('hand_salary', '<', 5000)])
+        hand_salary_status = self.env['rest.staff'].search(
+            [('hand_salary', '<', 5000)])
         for rec in hand_salary_status:
-            print("Full Name:############", rec.full_name, 'gender: $$$$$$$$$$', rec.gender)
+            _logger.info("The result is",hand_salary_status.hand_salary)
+            _logger.info("------------------------")
+            print("Full Name:############", rec.full_name,
+                  'gender: $$$$$$$$$$', rec.gender)
+            
+        _logger.warning( rec.full_name, rec.gender,"Unable to access the attachments of %s. Tried to decrypt it, but %s.")
+           
 
 
 class TestStringMethods(unittest.TestCase):
@@ -36,5 +45,5 @@ class TestAge(unittest.TestCase):
     def test_age(self):
         self.env['rest.staff'].val_age()
         self.assertAlmostEqual(self.test_age(19))
-        self.assertRaises(self.test_age(10), expected_exception="Age should greater then 18")
-
+        self.assertRaises(self.test_age(
+            10), expected_exception="Age should greater then 18")
